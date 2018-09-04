258c6b13f12eb6378e2c6701df97d1505db2a49b
Austin <austin8897@gmail.com>
2018-09-03 19:03:50 -0500

    Completed Assignment: Git Some Practice

---
+++ app.js
@@ -0,0 +1,19 @@
+/*  
+    Name: Austin Landers
+    Date: September 3rd, 2018
+    Assignment: Git Some Practice
+*/
+
+// 3, 2, 1... Code! - Coding Time!
+//Display name and age
+console.log("Austin, 21");
+//Do the operations: 6+3, 6-3, 6*3, 6/3
+console.log(6+3, 6-3, 6*3, 6/3);
+//What are the outputs for: "console.log(4 + 5);" "console.log("4 + 5");" and "console.log("4" + "5");"
+console.log(9, "4 + 5", 45);
+
+//Play with Variables - Coding Time!
+//Convert 32 Celcius to Fahrenheit
+let Celcius = 32;
+let Fahren = (Celcius * (9/5)) + 32;
+console.log(Fahren);
